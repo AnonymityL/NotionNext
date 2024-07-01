cd184ddffa7312b2692b8d79ff1bb95d9a0f178c
lishuo <my_ls@icloud.com>
2024-07-01 10:52:36 +0800

    update

--- blog.config.js
+++ blog.config.js
@@ -434,6 +434,7 @@ const BLOG = {
   // <---- 评论插件
 
   // ----> 站点统计
+  SPEED_INSIGHTS_VERCEL: process.env.NEXT_PUBLIC_SPEED_INSIGHTS_VERCEL || true,
   ANALYTICS_VERCEL: process.env.NEXT_PUBLIC_ANALYTICS_VERCEL || false, // vercel自带的统计 https://vercel.com/docs/concepts/analytics/quickstart https://github.com/tangly1024/NotionNext/issues/897
   ANALYTICS_BUSUANZI_ENABLE:
     process.env.NEXT_PUBLIC_ANALYTICS_BUSUANZI_ENABLE || true, // 展示网站阅读量、访问数 see http://busuanzi.ibruce.info/
